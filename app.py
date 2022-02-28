c958e5fc7108cbb5be10fbd13a012885d6ca2005
juli5567 <80360561+juli5567@users.noreply.github.com>
2022-02-28 17:12:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pycaret
 from pycaret.classification import *
 
 df= pd.read_csv("https://raw.githubusercontent.com/juli5567/OpsAnalytics/main/df.csv", delimiter= ";")
